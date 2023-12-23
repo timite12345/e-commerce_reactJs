5cfdbf712926c84d327e89d3bf0137f84028e34e
timite alassane <timite1145@gmail.com>
2023-12-23 06:11:04 +0100

    Site e-commerce avec reactJs

--- src/Pages/Accueil.js
+++ src/Pages/Accueil.js
@@ -1,9 +1,14 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronLeft, faChevronRight, faStar } from "@fortawesome/free-solid-svg-icons";
+import styled from "styled-components";
+
 export default function Accueil(){
 
+  
+    
   return(
 
 <body>
-
 
     <div id="template-mo-zay-hero-carousel" class="carousel slide" data-bs-ride="carousel">
         <ol class="carousel-indicators">
@@ -16,7 +21,7 @@ export default function Accueil(){
                 <div class="container">
                     <div class="row p-5">
                         <div class="mx-auto col-md-8 col-lg-6 order-lg-last">
-                            <img class="img-fluid" src="./img/banner_img_01.jpg" alt=""/>
+                            <img class="img-fluid" src="/img/banner_img_01.jpg" alt=""/>
                         </div>
                         <div class="col-lg-6 mb-0 d-flex align-items-center">
                             <div class="text-align-left align-self-center">
@@ -33,12 +38,13 @@ export default function Accueil(){
                         </div>
                     </div>
                 </div>
+                
             </div>
             <div class="carousel-item">
                 <div class="container">
                     <div class="row p-5">
                         <div class="mx-auto col-md-8 col-lg-6 order-lg-last">
-                            <img class="img-fluid" src="./img/banner_img_02.jpg" alt=""/>
+                            <img class="img-fluid" src="/img/banner_img_02.jpg" alt=""/>
                         </div>
                         <div class="col-lg-6 mb-0 d-flex align-items-center">
                             <div class="text-align-left">
@@ -57,7 +63,7 @@ export default function Accueil(){
                 <div class="container">
                     <div class="row p-5">
                         <div class="mx-auto col-md-8 col-lg-6 order-lg-last">
-                            <img class="img-fluid" src="./img/banner_img_03.jpg" alt=""/>
+                            <img class="img-fluid" src="/img/banner_img_03.jpg" alt=""/>
                         </div>
                         <div class="col-lg-6 mb-0 d-flex align-items-center">
                             <div class="text-align-left">
@@ -74,10 +80,10 @@ export default function Accueil(){
             </div>
         </div>
         <a class="carousel-control-prev text-decoration-none w-auto ps-3" href="#template-mo-zay-hero-carousel" role="button" data-bs-slide="prev">
-            <i class="fas fa-chevron-left"></i>
+        <button ><i class="fa fa-chevron-right"><FontAwesomeIcon icon={faChevronLeft} /></i></button>
         </a>
         <a class="carousel-control-next text-decoration-none w-auto pe-3" href="#template-mo-zay-hero-carousel" role="button" data-bs-slide="next">
-            <i class="fas fa-chevron-right"></i>
+        <button ><i class="fa fa-chevron-right"><FontAwesomeIcon icon={faChevronRight} /></i></button>
         </a>
     </div>
    
@@ -93,17 +99,17 @@ export default function Accueil(){
         </div>
         <div class="row">
             <div class="col-12 col-md-4 p-5 mt-3">
-                <a href="#"><img src="./img/category_img_01.jpg" class="rounded-circle img-fluid border"/></a>
+                <a href="#"><img src="/img/category_img_01.jpg" class="rounded-circle img-fluid border"/></a>
                 <h5 class="text-center mt-3 mb-3">Watches</h5>
                 <p class="text-center"><a class="btn btn-success">Go Shop</a></p>
             </div>
             <div class="col-12 col-md-4 p-5 mt-3">
-                <a href="#"><img src="./img/category_img_02.jpg" class="rounded-circle img-fluid border"/></a>
+                <a href="#"><img src="/img/category_img_02.jpg" class="rounded-circle img-fluid border"/></a>
                 <h2 class="h5 text-center mt-3 mb-3">Shoes</h2>
                 <p class="text-center"><a class="btn btn-success">Go Shop</a></p>
             </div>
             <div class="col-12 col-md-4 p-5 mt-3">
-                <a href="#"><img src="./img/category_img_03.jpg" class="rounded-circle img-fluid border"/></a>
+                <a href="#"><img src="/img/category_img_03.jpg" class="rounded-circle img-fluid border"/></a>
                 <h2 class="h5 text-center mt-3 mb-3">Accessories</h2>
                 <p class="text-center"><a class="btn btn-success">Go Shop</a></p>
             </div>
@@ -125,16 +131,16 @@ export default function Accueil(){
                 <div class="col-12 col-md-4 mb-4">
                     <div class="card h-100">
                         <a href="shop-single.html">
-                            <img src="./img/feature_prod_01.jpg" class="card-img-top" alt="..."/>
+                            <img src="/img/feature_prod_01.jpg" class="card-img-top" alt="..."/>
                         </a>
                         <div class="card-body">
                             <ul class="list-unstyled d-flex justify-content-between">
                                 <li>
-                                    <i class="text-warning fa fa-star"></i>
-                                    <i class="text-warning fa fa-star"></i>
-                                    <i class="text-warning fa fa-star"></i>
-                                    <i class="text-muted fa fa-star"></i>
-                                    <i class="text-muted fa fa-star"></i>
+                                    <i class="text-warning fa fa-star"><FontAwesomeIcon icon={faStar} /></i>
+                                    <i class="text-warning fa fa-star"><FontAwesomeIcon icon={faStar} /></i>
+                                    <i class="text-warning fa fa-star"><FontAwesomeIcon icon={faStar} /></i>
+                                    <i class="text-muted fa fa-star"><FontAwesomeIcon icon={faStar} /></i>
+                                    <i class="text-muted fa fa-star"><FontAwesomeIcon icon={faStar} /></i>
                                 </li>
                                 <li class="text-muted text-right">$240.00</li>
                             </ul>
@@ -149,16 +155,16 @@ export default function Accueil(){
                 <div class="col-12 col-md-4 mb-4">
                     <div class="card h-100">
                         <a href="shop-single.html">
-                            <img src="./img/feature_prod_02.jpg" class="card-img-top" alt="..."/>
+                            <img src="/img/feature_prod_02.jpg" class="card-img-top" alt="..."/>
                         </a>
                         <div class="card-body">
                             <ul class="list-unstyled d-flex justify-content-between">
                                 <li>
-                                    <i class="text-warning fa fa-star"></i>
-                                    <i class="text-warning fa fa-star"></i>
-                                    <i class="text-warning fa fa-star"></i>
-                                    <i class="text-muted fa fa-star"></i>
-                                    <i class="text-muted fa fa-star"></i>
+                                    <i class="text-warning fa fa-star"><FontAwesomeIcon icon={faStar} /></i>
+                                    <i class="text-warning fa fa-star"><FontAwesomeIcon icon={faStar} /></i>
+                                    <i class="text-warning fa fa-star"><FontAwesomeIcon icon={faStar} /></i>
+                                    <i class="text-muted fa fa-star"><FontAwesomeIcon icon={faStar} /></i>
+                                    <i class="text-muted fa fa-star"><FontAwesomeIcon icon={faStar} /></i>
                                 </li>
                                 <li class="text-muted text-right">$480.00</li>
                             </ul>
@@ -173,16 +179,16 @@ export default function Accueil(){
                 <div class="col-12 col-md-4 mb-4">
                     <div class="card h-100">
                         <a href="shop-single.html">
-                            <img src="./img/feature_prod_03.jpg" class="card-img-top" alt="..."/>
+                            <img src="/img/feature_prod_03.jpg" class="card-img-top" alt="..."/>
                         </a>
                         <div class="card-body">
                             <ul class="list-unstyled d-flex justify-content-between">
                                 <li>
-                                    <i class="text-warning fa fa-star"></i>
-                                    <i class="text-warning fa fa-star"></i>
-                                    <i class="text-warning fa fa-star"></i>
-                                    <i class="text-warning fa fa-star"></i>
-                                    <i class="text-warning fa fa-star"></i>
+                                    <i class="text-warning fa fa-star"><FontAwesomeIcon icon={faStar} /></i>
+                                    <i class="text-warning fa fa-star"><FontAwesomeIcon icon={faStar} /></i>
+                                    <i class="text-warning fa fa-star"><FontAwesomeIcon icon={faStar} /></i>
+                                    <i class="text-warning fa fa-star"><FontAwesomeIcon icon={faStar} /></i>
+                                    <i class="text-warning fa fa-star"><FontAwesomeIcon icon={faStar} /></i>
                                 </li>
                                 <li class="text-muted text-right">$360.00</li>
                             </ul>
@@ -197,105 +203,6 @@ export default function Accueil(){
             </div>
         </div>
     </section>
-   
-    <footer class="bg-dark" id="tempaltemo_footer">
-        <div class="container">
-            <div class="row">
-
-                <div class="col-md-4 pt-5">
-                    <h2 class="h2 text-success border-bottom pb-3 border-light logo">Zay Shop</h2>
-                    <ul class="list-unstyled text-light footer-link-list">
-                        <li>
-                            <i class="fas fa-map-marker-alt fa-fw"></i>
-                            123 Consectetur at ligula 10660
-                        </li>
-                        <li>
-                            <i class="fa fa-phone fa-fw"></i>
-                            <a class="text-decoration-none" href="tel:010-020-0340">010-020-0340</a>
-                        </li>
-                        <li>
-                            <i class="fa fa-envelope fa-fw"></i>
-                            <a class="text-decoration-none" href="mailto:info@company.com">info@company.com</a>
-                        </li>
-                    </ul>
-                </div>
-
-                <div class="col-md-4 pt-5">
-                    <h2 class="h2 text-light border-bottom pb-3 border-light">Products</h2>
-                    <ul class="list-unstyled text-light footer-link-list">
-                        <li><a class="text-decoration-none" href="#">Luxury</a></li>
-                        <li><a class="text-decoration-none" href="#">Sport Wear</a></li>
-                        <li><a class="text-decoration-none" href="#">Men's Shoes</a></li>
-                        <li><a class="text-decoration-none" href="#">Women's Shoes</a></li>
-                        <li><a class="text-decoration-none" href="#">Popular Dress</a></li>
-                        <li><a class="text-decoration-none" href="#">Gym Accessories</a></li>
-                        <li><a class="text-decoration-none" href="#">Sport Shoes</a></li>
-                    </ul>
-                </div>
-
-                <div class="col-md-4 pt-5">
-                    <h2 class="h2 text-light border-bottom pb-3 border-light">Further Info</h2>
-                    <ul class="list-unstyled text-light footer-link-list">
-                        <li><a class="text-decoration-none" href="#">Home</a></li>
-                        <li><a class="text-decoration-none" href="#">About Us</a></li>
-                        <li><a class="text-decoration-none" href="#">Shop Locations</a></li>
-                        <li><a class="text-decoration-none" href="#">FAQs</a></li>
-                        <li><a class="text-decoration-none" href="#">Contact</a></li>
-                    </ul>
-                </div>
-
-            </div>
-
-            <div class="row text-light mb-4">
-                <div class="col-12 mb-3">
-                    <div class="w-100 my-3 border-top border-light"></div>
-                </div>
-                <div class="col-auto me-auto">
-                    <ul class="list-inline text-left footer-icons">
-                        <li class="list-inline-item border border-light rounded-circle text-center">
-                            <a class="text-light text-decoration-none" target="_blank" href="http://facebook.com/"><i class="fab fa-facebook-f fa-lg fa-fw"></i></a>
-                        </li>
-                        <li class="list-inline-item border border-light rounded-circle text-center">
-                            <a class="text-light text-decoration-none" target="_blank" href="https://www.instagram.com/"><i class="fab fa-instagram fa-lg fa-fw"></i></a>
-                        </li>
-                        <li class="list-inline-item border border-light rounded-circle text-center">
-                            <a class="text-light text-decoration-none" target="_blank" href="https://twitter.com/"><i class="fab fa-twitter fa-lg fa-fw"></i></a>
-                        </li>
-                        <li class="list-inline-item border border-light rounded-circle text-center">
-                            <a class="text-light text-decoration-none" target="_blank" href="https://www.linkedin.com/"><i class="fab fa-linkedin fa-lg fa-fw"></i></a>
-                        </li>
-                    </ul>
-                </div>
-                <div class="col-auto">
-                    <label class="sr-only" for="subscribeEmail">Email address</label>
-                    <div class="input-group mb-2">
-                        <input type="text" class="form-control bg-dark border-light" id="subscribeEmail" placeholder="Email address"/>
-                        <div class="input-group-text btn-success text-light">Subscribe</div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div class="w-100 bg-black py-3">
-            <div class="container">
-                <div class="row pt-2">
-                    <div class="col-12">
-                        <p class="text-left text-light">
-                            Copyright &copy; 2021 Company Name 
-                            | Designed by <a rel="sponsored" href="https://templatemo.com" target="_blank">TemplateMo</a>
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-    </footer>
-   
-    <script src="assets/js/jquery-1.11.0.min.js"></script>
-    <script src="assets/js/jquery-migrate-1.2.1.min.js"></script>
-    <script src="assets/js/bootstrap.bundle.min.js"></script>
-    <script src="assets/js/templatemo.js"></script>
-    <script src="assets/js/custom.js"></script>
 </body>
 
   )
